eb955af98b8db514cb9910908e164d2b14116ba5
bruno-hrf <rodrigues_bruno@outlook.pt>
2022-03-03 15:43:51 +0100

    Add Math.PI and Math.Pow

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,25 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a; 
+            double r; 
+            double Volume;
+            double AreaSup;
+
+            double pi = Math.PI;
+
+
+
+            Console.WriteLine("Altura Cilindro: ");
+            a = double.Parse( Console.ReadLine());
+            Console.WriteLine("Raio Cilindro: ");
+            r = double.Parse(Console.ReadLine());
+
+            Volume = pi * Math.Pow(r,2) * a;  
+            AreaSup = 2 * pi * r * (r + a);
+
+            Console.WriteLine($"Volume do seu cilindro é igual a: {Volume}");
+            Console.WriteLine($"A Área de superfície do seu cilindro é igual a: {AreaSup}");
         }
     }
 }
