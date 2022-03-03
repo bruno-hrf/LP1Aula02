fb6f9f30d3d0d055008326d9c5b9980707e14e54
bruno-hrf <rodrigues_bruno@outlook.pt>
2022-03-03 14:40:50 +0100

    First operation

---
+++ IncDec/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 1;
+
+            s++;
+
+            Console.WriteLine(s);
+
+        }
+    }
+}
